7b8fbb8d060f15babba94a67c3a59542353804e1
Jordan Schultz <jmschultz@ucdavis.edu>
2024-02-02 14:22:26 -0800

    Cleaned up factory, removed unnecessary variables

--- classes/CacheFactory.php
+++ classes/CacheFactory.php
@@ -4,18 +4,12 @@ namespace Stanford\RedcapNotifications;
 
 class CacheFactory
 {
-    const REDIS = 'REDIS';
-
-    const DATABASE = 'DATABASE';
-    public static function getCacheClient($type, $host = '', $port = '')
+    public static function getCacheClient($host = '', $port = '')
     {
-        if($type == self::REDIS and $port != '' and $host != ''){
+        if(!empty($host) and !empty($port)){
             return new Redis($host, $port);
-        }else{
-            if($type == self::REDIS){
-                \REDCap::logEvent('Redis host and/or port is missing!');
-            }
-            return new Database();
         }
+        return new Database();
     }
+
 }
