d5dc4888387cfb9039fa6d61b3e3328750d8d04d
Jordan Schultz <jmschultz@ucdavis.edu>
2024-02-02 14:23:07 -0800

    Added delete and list keys

--- classes/Redis.php
+++ classes/Redis.php
@@ -11,60 +11,47 @@ class Redis implements CacheInterface
 
     public function __construct($redisHost, $redisPort)
     {
-        // TODO: Get Predis client here.
         $this->client = new \Predis\Client([
             'host' => $redisHost,
             'port' => $redisPort,
             'connections' => 'relay' //For performance improvements
         ]);
-
     }
 
-    /**
-     * Set key value pair
-     * @param $key
-     * @param $value
-     * @return \Predis\Response\Status
-     */
     public function setKey($key, $value)
     {
         return $this->client->set($key, $value);
     }
 
-    /**
-     * Given an array of key/value pairs, set cache
-     * @param array $arr
-     * @return mixed
-     */
     public function setKeys(array $arr)
     {
         return $this->client->mset($arr);
     }
 
-    /**
-     * Get value for a given key
-     * @param $key
-     * @return string|null
-     */
     public function getKey($key)
     {
         return $this->client->get($key);
     }
 
-    /**
-     * Given an array of keys, return all values
-     * @param array $arr
-     * @return array
-     */
     public function getKeys(array $arr)
     {
         return $this->client->mget($arr);
     }
 
+
     public function deleteKey($key)
     {
         return $this->client->del($key);
-        // TODO: Implement deleteKey() method.
+    }
+
+    public function deleteKeys(array $arr)
+    {
+        return $this->client->del($arr);
+    }
+
+    public function listKeys(string $pattern)
+    {
+        return $this->client->keys($pattern);
     }
 
     public function expireKey($key)
